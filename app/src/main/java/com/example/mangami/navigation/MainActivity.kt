cabee3b14ca58664b0bd0c6eb12d667a84729384
leozinho <leonardoschwedler@gmail.com>
2025-04-13 14:31:31 +0200

    feat: Implement main navigation graph with bottom navigation and toolbar
    This commit implements a main navigation graph with bottom navigation for home, profile, and settings fragments. It also sets up a toolbar that interacts with the navigation controller, displaying the back button when not on a top-level destination.
    - Adds navigation graphs for home, profile, and settings features.
    - Creates a main navigation graph including the individual feature graphs.
    - Updates the main activity layout to include a toolbar and a bottom navigation view.
    - Implements MainActivity with bottom navigation setup, toolbar integration, and navigation controller.
    - Adds a back arrow icon for the toolbar.
    - Modifies the bottom navigation menu to align with the new navigation graph.

--- app/src/main/java/com/example/mangami/navigation/MainActivity.kt
+++ app/src/main/java/com/example/mangami/navigation/MainActivity.kt
@@ -3,30 +3,49 @@ package com.example.mangami.navigation
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.mangami.R
 import com.example.mangami.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private lateinit var navController: NavController
-    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.toolbar) { view, insets ->
+            val topInset = insets.getInsets(WindowInsetsCompat.Type.systemBars()).top
+            view.updatePadding(top = topInset)
+            insets
+        }
         setupBottomNavigation()
     }
 
     private fun setupBottomNavigation() {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.MainfragmentContainerView) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.MainfragmentContainerView) as NavHostFragment
         navController = navHostFragment.navController
-        bottomNavigationView = binding.bottomNavigation
-        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.homeFragment, R.id.profileFragment, R.id.settingsFragment)
+        )
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+        binding.bottomNavigation.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener() { _, destination, _ ->
+            val isTopLevelDestination =
+                appBarConfiguration.topLevelDestinations.contains(destination.id)
+            if (!isTopLevelDestination) {
+                binding.toolbar.setNavigationIcon(R.drawable.ic_arrow_back)
+            }
+        }
     }
 }
